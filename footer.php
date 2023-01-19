dfc1741c3dae0cab87f12b6e582a8a2a3f095f23
indrasuryajayadi <121953212+indrasuryajayadi@users.noreply.github.com>
2023-01-19 15:46:01 +0800

    add footer.php
    menambahkan footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="d-flex flex-column bg-dark">
+        <!-- Copyright -->
+        <div class="text-white mb-3 mb-md-0 text-center">
+            <p class="card-text text-center mt-3 mb-3"> INERSYSUNIVERSITY2023 &#169;All rights reserved.</p>
+        </div>
+    </div>
+</footer>
