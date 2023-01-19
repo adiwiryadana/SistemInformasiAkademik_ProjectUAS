1527fa4257597cb5ea3bae3c1e2a120ec811da19
rondan25 <121587986+rondan25@users.noreply.github.com>
2023-01-19 16:12:29 +0800

    Update biodata_dosen.php
    menghapus bagian code yang tidak di gunakan

--- dosen/biodata_dosen.php
+++ dosen/biodata_dosen.php
@@ -37,41 +37,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 
-// // include untuk koneksi ke database
-// include "koneksi.php";
 
-// //  Fungsi untuk mencegah inputan karakter yang tidak sesuai
-//  function input($data) {
-//     $data = trim($data);
-//     $data = stripslashes($data);
-//     $data = htmlspecialchars($data);
-//     return $data;
-// }
-
-// // cek nilai dari method get 
-// if (isset($_GET['nim'])) {
-//     $nim=input($_GET["nim"]);
-//     $sql="select * from dosen where nim=$nim";
-//     $hasil=mysqli_query($koneksi,$sql);
-//     $data = mysqli_fetch_assoc($hasil);
-// }
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $nim=input($_POST["nim"]);
-//     $nama=input($_POST["nama"]);
-//     $no_hp=input($_POST["no_hp"]);
-//     $alamat=input($_POST["alamat"]);
-
-
-//      // Query update data pada tabel anggota
-    //  $sql="update dosen set
-    //  nama='$nama',
-    //  no_hp='$no_hp',
-    //  alamat='$alamat'
-    //  where nim=$nim";
-
-//       //Mengeksekusi query diatas
-//     $hasil=mysqli_query($koneksi,$sql);
-
-// }
 ?>
