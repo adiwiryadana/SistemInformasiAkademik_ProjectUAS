7a23228be8143fc1ccaa10ef6409aa5787a11738
rondan25 <121587986+rondan25@users.noreply.github.com>
2023-01-19 15:48:03 +0800

    cekdosen.php

---
+++ cekdosen.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+//cek apakah user sudah login
+if(!isset($_SESSION['username'])){
+    die("Anda belum login");
+}
+
+//cek level user
+if($_SESSION['level']!="Dosen"){
+    die("Anda bukan Dosen");
+}
+?>
