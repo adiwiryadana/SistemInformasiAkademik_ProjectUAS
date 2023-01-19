e7b9fb80016337e8c331cdfbf2c88b764b31cbdc
wiranatakrisna <86475930+wiranatakrisna@users.noreply.github.com>
2023-01-19 15:57:34 +0800

    Add class_upDataMhs.php
    menambahkan file php untuk edit/update data mahasiswa

---
+++ mahasiswa/class_upDataMhs.php
@@ -0,0 +1,36 @@
+<?php 
+include('../koneksi.php');
+include('../admin/user panel/class_getNim.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Nim=htmlspecialchars($_POST["nim"]);
+    $tempat_lahir = ($_POST["tempat_lahir"]);
+    $tanggal_lahir = ($_POST["tanggal_lahir"]);
+    $gender = ($_POST["gender"]);
+    $phone = ($_POST["phone"]);
+    $email = ($_POST["email"]);
+    $ayah = ($_POST["ayah"]);
+    $ibu = ($_POST["ibu"]);
+    $prodi = ($_POST["prodi"]);
+    $alamat = ($_POST["alamat"]);
+
+    $db = new koneksi();
+    $koneksi = $db->getKoneksi();
+
+    $sql = "UPDATE mahasiswa SET tempat_lahir='$tempat_lahir', tanggal_lahir='$tanggal_lahir',
+        gender='$gender', phone='$phone', email='$email', ayah='$ayah',
+        ibu='$ibu', prodi='$prodi', alamat='$alamat' WHERE nim='$Nim'";
+
+        $hasil =  mysqli_query($koneksi, $sql);
+
+        if($hasil) {
+            header("Location:mahasiswa.php");
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Gagal Melakukan Update!")';
+            echo '</script>';
+            header("Location:mahasiswa.php");
+        }
+}
+
+?>
