177fcaff4c1eece2716e5a50f00f593d215ef33c
indrasuryajayadi <121953212+indrasuryajayadi@users.noreply.github.com>
2023-01-21 21:30:28 +0800

    Update class_getNim.php
    membuat dokumentasi pada class_getNim.php

--- admin/user panel/class_getNim.php
+++ admin/user panel/class_getNim.php
@@ -1,8 +1,11 @@
 <?php 
 
-class getNim {
+class getNim { //membuat dan mendeklarasikan class getNim yang dimana didalamnya terdapat properti dan juga method
+    
+    //membuat properti $nim yang dinyatakan sebagai public (seluruh kode program di luar class bisa mengaksesnya)
     public $nim;
 
+    //membuat method getNim () yang dinyatakan sebagai public
     public function getNim()
     {
         if(isset($_GET['nim'])){
