178fc73707e5ab42d5f8e6c5f6e66b5e9f3f3053
rondan25 <121587986+rondan25@users.noreply.github.com>
2023-01-21 21:21:51 +0800

    Update class_jadwal.php
    mengisikan dokumentasi pada class_jadwal.php

--- admin/schedule panel/class_jadwal.php
+++ admin/schedule panel/class_jadwal.php
@@ -1,9 +1,10 @@
 <?php
+// koneksi ke database
 include('../../koneksi.php');
-
 $db = new koneksi();
 $koneksi = $db->getKoneksi();
 
+//Cek apakah ada kiriman form dari method post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $kode_kelas = ($_POST["kode_kelas"]);
     $kode_mk = ($_POST["kode_mk"]);
@@ -11,10 +12,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $hari = ($_POST["hari"]);
     $waktu = ($_POST["waktu"]);
 }
-
+//query untuk menambahkan data pada tabel jadwal
 $query = "INSERT INTO jadwal (kode_mk,kode_kelas,ruang,hari,waktu) VALUES ('$kode_mk', '$kode_kelas', '$ruang', '$hari', '$waktu')";
 $hasil = mysqli_query($koneksi, $query);
-
+//mengecek apa query berhasil di jalankan
 if($hasil) {
     header("Location: jadwal.php?kode_kelas=$kode_kelas");
 } else {
