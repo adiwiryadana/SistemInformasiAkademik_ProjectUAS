fd4626da8278b5ab32547f178d7fb901e4ebc507
wiranatakrisna <86475930+wiranatakrisna@users.noreply.github.com>
2023-01-21 21:22:43 +0800

    Update class_delete.php

--- admin/matakuliah panel/class_delete.php
+++ admin/matakuliah panel/class_delete.php
@@ -1,10 +1,11 @@
-<?php
+<?php //menyertakan file koneksi.php untuk terkoneksi ke db
 include('../../koneksi.php');
 
+//membuat class delMatakuliah dimana terdapat properti dan juga method
 class delMatakuliah{
 
-    public $kode_mk;
-    public $data;
+    public $kode_mk; //membuat properti $kodemk yang bersifat public
+    public $data; //membuat properti $data yang bersifat public
 
 
     public function __construct()
@@ -13,11 +14,11 @@ class delMatakuliah{
             $this->kode_mk = $_GET['kode_mk'];
         }
     }
-
+    //membuat unction delete() yang bersifat public untuk melakukan DELETE mata kuliah
     public function delete(){
         $db = new koneksi();
         $koneksi = $db->getKoneksi();
-
+        //query untuk melakukan delete 
         $this->data = mysqli_query($koneksi, "DELETE FROM matakuliah WHERE kode_mk = '$this->kode_mk'");
 
         if($this->data) {
