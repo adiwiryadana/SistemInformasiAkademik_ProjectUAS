be5211f3a740b65e1df0cc53c2cd6031eeee5c92
wiranatakrisna <86475930+wiranatakrisna@users.noreply.github.com>
2023-01-21 21:38:42 +0800

    Update class_tambah.php

--- admin/matakuliah panel/class_tambah.php
+++ admin/matakuliah panel/class_tambah.php
@@ -1,13 +1,17 @@
 <?php
+//menyertakan file koneksi.php untuk terhubung ke db
 include('../../koneksi.php');
 
+//membuat class untuk menambah mata kuliah
 class addMatakuliah {
+    //deklarasi variable public
     public $kode_mk;
     public $nama_mk;
     public $sks;
     public $nim;
     public $query;
-
+    
+    //membuat function_construct bersifat public
     public function __construct(){
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $this->kode_mk = ($_POST["kode_mk"]);
@@ -16,11 +20,11 @@ class addMatakuliah {
             $this->nim= ($_POST["nim"]);
         }
     }
-
+    //membuat function add() untuk menambahkan matakuliah
     public function add(){
         $db = new koneksi();
         $koneksi = $db->getKoneksi();
-
+    //query untuk menambahkan data matakuliah
         $this->query = $hasil= mysqli_query($koneksi, "INSERT INTO matakuliah (kode_mk, nama_mk, sks, nim) VALUES ('$this->kode_mk', '$this->nama_mk', '$this->sks', '$this->nim')");
 
         if($hasil){
