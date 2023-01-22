ed44423e0993d118a3070570da8d1b4b90666d67
Komang Adi Wirya Dana <103612647+adiwiryadana@users.noreply.github.com>
2023-01-22 10:53:49 +0800

    Update class_addNilai.php
    update rata rata dan nilai akhir mahasiswa

--- admin/grade panel/class_addNilai.php
+++ admin/grade panel/class_addNilai.php
@@ -10,6 +10,8 @@ class addNilai {
     public $tugas;
     public $query;
     public $hasil;
+    public $rata_rata;
+    public $nilai_akhir;
 
 
     public function  __construct(){
@@ -26,8 +28,19 @@ class addNilai {
     public function add(){
         $db = new koneksi();
         $koneksi = $db->getKoneksi();
-
-        $this->query = "INSERT INTO nilai (nim,kode_mk,uas,uts,tugas,kuis) VALUES ('$this->nim', '$this->kode_mk', $this->uas, $this->uts, $this->kuis, $this->tugas)";
+        
+        $this->rata_rata = ($this->uas + $this->uts + $this->kuis + $this->tugas) / 4;
+        
+        if ($this->rata_rata >= 88) {
+            $this->nilai_akhir = 'A';
+        } else if ($this->rata_rata >= 80) {
+            $this->nilai_akhir = 'B';
+        } else if ($this->rata_rata >= 60) {
+            $this->nilai_akhir = 'C';
+        } else if ($this->rata_rata < 60) {
+            $this->nilai_akhir = 'D';
+        }
+        $this->query = "INSERT INTO nilai (nim,kode_mk,uas,uts,tugas,kuis,rata_rata,nilai_akhir) VALUES ('$this->nim', '$this->kode_mk', $this->uas, $this->uts, $this->tugas, $this->kuis, $this->rata_rata, '$this->nilai_akhir')";
         $this->hasil = mysqli_query($koneksi, $this->query);
         if($this->hasil){
             header("Location: class_nilai.php?nim=$this->nim");
