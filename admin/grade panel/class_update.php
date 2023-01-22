a8c2fe153078600c3d882878ab30dd37bc3cbe76
Komang Adi Wirya Dana <103612647+adiwiryadana@users.noreply.github.com>
2023-01-22 11:08:16 +0800

    Update class_update.php
    menambahkan rata rata dan nilai akhir

--- admin/grade panel/class_update.php
+++ admin/grade panel/class_update.php
@@ -12,6 +12,8 @@ class updateNilai {
     public $tugas;
     public $kuis;
     public $query;
+    public $rata_rata;
+    public $nilai_akhir;
 
     public function __construct(){  
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -23,13 +25,25 @@ class updateNilai {
             $this->kuis= ($_POST["kuis"]);
         }
     }
-
+    
     public function update(){
         $db = new koneksi();
         $koneksi = $db->getKoneksi();
-
+        
+        $this->rata_rata = ($this->uas + $this->uts + $this->kuis + $this->tugas) / 4;
+        
+        if ($this->rata_rata >= 88) {
+            $this->nilai_akhir = 'A';
+        } else if ($this->rata_rata >= 80) {
+            $this->nilai_akhir = 'B';
+        } else if ($this->rata_rata >= 60) {
+            $this->nilai_akhir = 'C';
+        } else if ($this->rata_rata < 60) {
+            $this->nilai_akhir = 'D';
+        }
+        
         $this->query = mysqli_query($koneksi, "UPDATE nilai SET uas='$this->uas', uts='$this->uts', 
-        tugas='$this->tugas', kuis='$this->kuis' WHERE kode_mk='$this->kode_mk' AND nim='$this->nim'");
+        tugas='$this->tugas', kuis='$this->kuis', rata_rata='$this->rata_rata' , nilai_akhir='$this->nilai_akhir' WHERE kode_mk='$this->kode_mk' AND nim='$this->nim'");
 
         if($this->query){
             header("Location: class_nilai.php?nim=$this->nim");
